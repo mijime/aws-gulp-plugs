e9744b76dff01ae50b79249e457eed945af67ced
mijime <07cc0110+gthb@gmail.com>
2016-07-13 16:01:18 +0900

    :art: Add task insert to dynamodb from json

---
+++ src/dynamodb.js
@@ -0,0 +1,49 @@
+import path from 'path';
+import {obj} from 'through2';
+import {DynamoDB} from 'aws-sdk';
+
+export default function batchWriteItem(...args) {
+  const dynamodb = new DynamoDB(...args);
+
+  return obj(function transform(file, enc, done) {
+    const TableName = path.basename(file.path, '.json');
+    const records = JSON.parse(file.contents.toString(enc));
+    const putRequests = records.map(record => {
+      const Item = Object.keys(record).map(key => {
+        const k = key.split(' ');
+        const kName = k[0];
+        const kType = k[1];
+        const value = record[key];
+
+        if (!value) {
+          return {};
+        }
+
+        switch (kType) {
+          case '(M)':
+            return {key: kName, value: {M: JSON.parse(record[key])}};
+          default:
+            return {key: kName, value: {S: record[key]}};
+        }
+      }).reduce((acc, {key, value}) => {
+        if (!key || !value) {
+          return acc;
+        }
+
+        acc[key] = value;
+        return acc;
+      }, {});
+
+      return {PutRequest: {Item}};
+    });
+    const RequestItems = {};
+    RequestItems[TableName] = putRequests;
+
+    dynamodb.batchWriteItem({RequestItems}, err => {
+      if (err) {
+        return done(err);
+      }
+      return done();
+    });
+  });
+}
